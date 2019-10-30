fcc7053b1a4e637f85854d5a313b917c46e1606c
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-30 14:49:05 +0000

    refactored ValidateUser and created validatePost middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -46,21 +46,26 @@ function validateUserId(req, res, next) {
     }
   });
 }
-
 function validateUser(req, res, next) {
-  if (Object.keys(req.body).length) {
-    if (Object.keys(req.body).includes("name")) {
-      next();
+    if (!Object.keys(req.body).length) {
+      res.status(400).json({ message: "missing user data!" });
+    } else if (!req.body.name) {
+      res.status(400).json({ message: 'missing required "name" field!' });
     } else {
-      res.status(400).json({ message: "missing required name field" });
+      next();
     }
+  }
+  
+function validatePost(req, res, next) {
+  if (!Object.keys(req.body).length) {
+    res.status(400).json({ message: "missing user data!" });
+  } else if (!req.body.text) {
+    res.status(400).json({
+      message: "missing required text field"
+    });
   } else {
-    res.status(400).json({ message: "missing user data" });
+    next();
   }
 }
-
-// function validatePost(req, res, next) {
-
-// };
 
 module.exports = router;
