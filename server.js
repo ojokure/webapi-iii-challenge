d3aec9f9d0fa5efdc05e35eec46164dc4352d9b4
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-30 13:25:18 +0000

    created a logger middleware  logs to the console the following information about each request: request method, request url, and a timestamp

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const express = 'express';
+const express = require("express");
+
+const helmet = require("helmet");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.originalUrl} ${req.headers['Date']}`);
+
+  next();
+}
+
+server.use(logger)
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
+// function logger(req, res, next) {
+//   console.log(`${req.method} ${req.originalUrl}`);
 
-};
+//   next();
+// }
 
 module.exports = server;
